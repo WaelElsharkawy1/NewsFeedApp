881602e7bb2fc2f3bc124cff7de4119e5b6c69e6
WaelElsharkawy1 <wael.elsharkawy.9@gmail.com>
2018-09-06 11:57:01 +0200

    NewsApp Second Edition

--- app/src/main/java/com/example/waelelsharkawy/newsfeedapp/NewsActivity.java
+++ app/src/main/java/com/example/waelelsharkawy/newsfeedapp/NewsActivity.java
@@ -4,11 +4,15 @@ import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -17,8 +21,9 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NewsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<News>> {
-    private static final String USGS_REQUEST_URL = "https://content.guardianapis.com/search?show-tags=contributor/order-by=newest&api-key=1adcce6b-f5be-471b-9d10-560eca76af44";
+public class NewsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<News>>,
+        SharedPreferences.OnSharedPreferenceChangeListener {
+    private static final String USGS_REQUEST_URL = "https://content.guardianapis.com/search";
     private static final int NEWSAPP_LOADER_ID = 1;
     private NewsAdapter mAdapter;
     private TextView mEmptyStateTextView;
@@ -33,6 +38,13 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
         NewsListView.setEmptyView(mEmptyStateTextView);
         mAdapter = new NewsAdapter(this, new ArrayList<News>());
         NewsListView.setAdapter(mAdapter);
+
+        // Obtain a reference to the SharedPreferences file for this app
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        // And register to be notified of preference changes
+        // So we know when the user has adjusted the query settings
+        prefs.registerOnSharedPreferenceChangeListener(this);
+
         NewsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
@@ -61,7 +73,24 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
-        return new NewsLoader(this, USGS_REQUEST_URL);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String resultSize = sharedPrefs.getString(
+                getString(R.string.settings_result_size_key),
+                getString(R.string.settings_result_size_default));
+
+        String section_type = sharedPrefs.getString(
+                getString(R.string.settings_section_type_key),
+                getString(R.string.settings_section_type_default)
+        );
+
+        Uri baseUri = Uri.parse(USGS_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("show-tags", "contributor");
+        uriBuilder.appendQueryParameter("api-key", "1adcce6b-f5be-471b-9d10-560eca76af44");
+        uriBuilder.appendQueryParameter("section", section_type);
+        uriBuilder.appendQueryParameter("page-size", resultSize);
+        return new NewsLoader(this, uriBuilder.toString());
     }
 
     @Override
@@ -77,5 +106,43 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public void onLoaderReset(Loader<List<News>> loader) {
         mAdapter.clear();
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        if (s.equals(getString(R.string.settings_result_size_key)) ||
+                s.equals(getString(R.string.settings_section_type_key))) {
+            // Clear the ListView as a new query will be kicked off
+            mAdapter.clear();
+
+            // Hide the empty state text view as the loading indicator will be displayed
+            mEmptyStateTextView.setVisibility(View.GONE);
+
+            // Show the loading indicator while new data is being fetched
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.VISIBLE);
+
+            // Restart the loader to requery the USGS as the query settings have been updated
+            getLoaderManager().restartLoader(NEWSAPP_LOADER_ID, null, this);
+
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
